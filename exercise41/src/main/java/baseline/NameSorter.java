53dba91d50f319c1b8eb1793ae29568028e1c4e6
RyanTheGuyInSpace <ryo_hodge@hotmail.com>
2021-10-12 19:12:29 -0400

    Added pseudocode and skeleton for solution 41

---
+++ exercise41/src/main/java/baseline/NameSorter.java
@@ -0,0 +1,35 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Ryan Hodge
+ */
+
+package baseline;
+
+import java.util.ArrayList;
+
+public class NameSorter {
+    ArrayList<String> names;
+
+    /**
+     * Reads names from the specified file and stores them inside the names variable.
+     * @param fileName The name of the file to read the names from.
+     */
+    private void readNames(String fileName) {
+
+    }
+
+    /**
+     * Sorts the names variable alphabetically.
+     */
+    private void sortNames() {
+
+    }
+
+    /**
+     * Writes all data inside the names variable to the specified file.
+     * @param fileName The name of the file to write the names into.
+     */
+    private void writeNames(String fileName) {
+
+    }
+}
---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Ryan Hodge
+ */
+
+package baseline;
+
+public class Solution41 {
+    public static void main(String[] args) {
+
+        // Create instance of NameSorter
+        // Open file to read names from
+        // Use NameSorter to read in names from the input file and store them inside the names variable
+        // Sort the names inside the NameSorter names variable
+        // Print the sorted list of names into an output file along with a line counting the number of names written
+
+    }
+}
