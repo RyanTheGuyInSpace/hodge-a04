b4a2c7128e249a0c4d1f5a198505f6c1ffcffc0a
RyanTheGuyInSpace <ryo_hodge@hotmail.com>
2021-10-16 13:08:11 -0400

    Added pseudocode and skeleton for solution 42

---
+++ exercise42/src/main/java/baseline/EmployeeReader.java
@@ -0,0 +1,36 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Ryan Hodge
+ */
+
+package baseline;
+
+import java.util.LinkedList;
+
+public class EmployeeReader {
+    public LinkedList<Employee> employees;
+
+    /**
+     * Reads the specified file for employees and stores them into the employees list.
+     * @param fileName The name of the file to read Employees from.
+     */
+    public void readEmployees(String fileName) {
+
+    }
+
+    /**
+     * Saves the specified employee to the employees list.
+     * @param employee The employee to add to the employees list.
+     */
+    public void saveEmployee(Employee employee) {
+
+    }
+
+    /**
+     * Writes all employees to the console and the specified file as a formatted table.
+     * @param fileName The name of the file to write the formatted table to.
+     */
+    public void writeEmployees(String fileName) {
+
+    }
+}
---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Ryan Hodge
+ */
+
+package baseline;
+
+public class Solution42 {
+    public static void main(String[] args) {
+        // Declare something to read the text file
+        // Declare an instance of EmployeeReader
+        // Use EmployeeReader to read in all the employees
+        // Print the employees back out
+    }
+}
