b3a8b9c7a1994fafe0163523e9b322d615a47c84
RyanTheGuyInSpace <ryo_hodge@hotmail.com>
2021-10-16 23:57:23 -0400

    Added pseudocode and skeleton for exercise 43

---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Ryan Hodge
+ */
+
+package baseline;
+
+public class Solution43 {
+    public static void main(String[] args) {
+        // Declare a WebsiteGenerator
+        // Prompt the user for the name of the website they want to create
+        // Prompt the user for the website's author's name
+        // Ask the user if they want a folder for css files
+        // Ask the user if they want a folder for js files
+        // Use the website generator to generate a website using the parameters provided by the user
+
+    }
+}
---
+++ exercise43/src/main/java/baseline/WebsiteGenerator.java
@@ -0,0 +1,42 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Ryan Hodge
+ */
+
+package baseline;
+
+import java.io.FileWriter;
+
+public class WebsiteGenerator {
+    private FileWriter writer;
+    public String websiteName;
+
+    /**
+     * Creates the base folder that will contain the website's files.
+     */
+    public void generateWebsiteDirectory() {
+
+    }
+
+    /**
+     * Creates an index.html file for the website that contains the website's name and the author's name.
+     * @param authorName The name of the website author
+     */
+    public void generateIndex(String authorName) {
+
+    }
+
+    /**
+     * Generates the CSS folder for the website.
+     */
+    public void generateWebsiteCSSDir() {
+
+    }
+
+    /**
+     * Generates the JS folder for the website.
+     */
+    public void generateWebsiteJSDir() {
+
+    }
+}
