026bf42c17a6fdc1608667872ca45103640603a0
RyanTheGuyInSpace <ryo_hodge@hotmail.com>
2021-10-17 23:05:09 -0400

    Added pseudocode and skeleton for exercise 45

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Ryan Hodge
+ */
+
+package baseline;
+
+public class Solution45 {
+    public static void main(String[] args) {
+        // Declare a new instance of a WordFinder
+        // Use the WordFinder to read the example file
+        // Use the WordFinder to replace every instance of the target word
+        // Print the content string of WordFinder back into a target file
+
+    }
+}
---
+++ exercise45/src/main/java/baseline/WordFinder.java
@@ -0,0 +1,40 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Ryan Hodge
+ */
+
+package baseline;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+public class WordFinder {
+    private BufferedReader reader;
+    private BufferedWriter writer;
+    public String content;
+
+    /**
+     * Writes the content String into the target file. The file gets generated if it doesn't already exist.
+     * @param fileName The name of the file to write the content String into.
+     */
+    public void writeToFile(String fileName) {
+
+    }
+
+    /**
+     * Reads the entirety of the specified file into the content String.
+     * @param fileName The name of the file to read into the content String.
+     */
+    public void readContent(String fileName) {
+
+    }
+
+    /**
+     * Replaces all instances of the targetWord with the replacementWord in the content String.
+     * @param targetWord The target that will be replaced by the replacementWord.
+     * @param replacementWord The word that will replace every instance of the targetWord.
+     */
+    public void replaceWord(String targetWord, String replacementWord) {
+
+    }
+}
